e2d9708218547806300a89c7df202611d924ff09
GuiSantos <gui.ssilva1994@gmail.com>
2026-02-17 16:52:18 -0300

    starting a spring project

---
+++ spring-order-project/src/main/java/com/example/demo/SpringOrderProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOrderProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOrderProjectApplication.class, args);
+	}
+
+}
