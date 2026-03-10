972a458cff8c2af093e514de4d8d8f71328f3d39
GuiSantos <gui.ssilva1994@gmail.com>
2026-03-09 22:18:34 -0300

    handling resource not found and database exceptions on User delete

--- spring-order-project/src/main/java/com/example/spring_order_project/resources/exceptions/ResourceExceptionHandler.java
+++ spring-order-project/src/main/java/com/example/spring_order_project/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.example.spring_order_project.services.exceptions.DatabaseException;
 import com.example.spring_order_project.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -18,6 +19,21 @@ public class ResourceExceptionHandler {
     public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
         HttpStatus status = HttpStatus.NOT_FOUND;
         String error = "Resource not found";
+
+        StandardError err = new StandardError(
+            Instant.now(), 
+            status.value(), 
+            error, 
+            e.getMessage(), 
+            request.getRequestURI());
+        
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        String error = "Database error";
 
         StandardError err = new StandardError(
             Instant.now(), 
