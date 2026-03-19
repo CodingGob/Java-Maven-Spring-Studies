33b5a10855e356c7d67270d3c750d8c195bb4045
GuiSantos <gui.ssilva1994@gmail.com>
2026-03-19 20:15:26 -0300

    adding configuration class to input data

---
+++ spring-mongodb-socialmedia-project/src/main/java/com/gobdev/spring_mongodb_social_api/config/Instantiation.java
@@ -0,0 +1,28 @@
+package com.gobdev.spring_mongodb_social_api.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.gobdev.spring_mongodb_social_api.domain.User;
+import com.gobdev.spring_mongodb_social_api.repositories.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner{
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        User maria = new User(null, "Maria Brown", "maria@gmail.com"); 
+        User alex = new User(null, "Alex Green", "alex@gmail.com"); 
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userRepository.deleteAll();
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+    }
+
+}
